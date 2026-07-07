ab71d6833eac143ee13734c0d865a90c6acbeb49
kamanaboina shasheesh <shasheesh20@gmail.com>
2026-07-08 00:54:06 +0530

    Update Doctor.h

--- Hospital-patient-record_system/include/Doctor.h
+++ Hospital-patient-record_system/include/Doctor.h
@@ -1,1 +1,43 @@
+#ifndef DOCTOR_H
+#define DOCTOR_H
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+//============================================================
+//                 Doctor Class
+// Purpose:
+// Stores doctor information and provides functions to
+// display, save, and load doctor records.
+//============================================================
+
+class Doctor
+{
+private:
+    int id;
+    string name;
+    string specialization;
+
+public:
+
+    // Constructors
+    Doctor();
+    Doctor(int doctorId, string doctorName, string doctorSpecialization);
+
+    // Getter Functions
+    int getId() const;
+    string getName() const;
+    string getSpecialization() const;
+
+    // Display Function
+    void displayDoctor() const;
+
+    // File Handling Functions
+    void saveDoctor(ofstream &out) const;
+    void loadDoctor(ifstream &in);
+};
+
+#endif
